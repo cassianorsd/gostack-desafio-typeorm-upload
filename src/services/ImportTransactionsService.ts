50d87eba7decc18a0bf2447e3ee3d88bd1eb8169
cassianorsd <cassiano.rsd@gmail.com>
2020-04-18 21:33:29 -0300

    passed tests again

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,26 +1,66 @@
 import csv from 'csvtojson';
 import fs from 'fs';
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 import uploadConfig from '../config/upload';
-import CreateTransactionService from './CreateTransactionService';
+import CreateCategoryService from './CreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class ImportTransactionsService {
   public async execute(file: Express.Multer.File): Promise<Transaction[]> {
     let transactions: Transaction[] = [];
-    const data = await csv().fromFile(
+    let data = await csv().fromFile(
       `${uploadConfig.directory}/${file.filename}`,
     );
-    transactions = await Promise.all(
-      data.map(transaction => {
-        const createTransaction = new CreateTransactionService();
-        return createTransaction.execute({
-          category: transaction.category,
-          title: transaction.title,
-          value: transaction.value,
-          type: transaction.type,
+
+    const updateCategory = async (transaction: Request): Promise<Request> => {
+      const categoriesRepository = getRepository(Category);
+      let hasCategory = await categoriesRepository.findOne({
+        where: { title: transaction.category },
+      });
+      if (!hasCategory) {
+        const createCategory = new CreateCategoryService();
+        hasCategory = await createCategory.execute({
+          title: transaction.category,
         });
+      }
+      return { ...transaction, category: hasCategory.id };
+    };
+
+    data = await Promise.all(
+      data.map(transaction => updateCategory(transaction)),
+    );
+
+    transactions = await Promise.all(
+      data.map(item => {
+        const transactionsRepository = getRepository(Transaction);
+        const transaction = transactionsRepository.create({
+          title: item.title,
+          value: item.value,
+          type: item.type,
+          category_id: item.category,
+        });
+        return transactionsRepository.save(transaction);
       }),
     );
+    // transactions = await Promise.all(
+    //   data.map(transaction => {
+    //     const createTransaction = new CreateTransactionService();
+    //     return createTransaction.execute({
+    //       category: transaction.category,
+    //       title: transaction.title,
+    //       value: transaction.value,
+    //       type: transaction.type,
+    //     });
+    //   }),
+    // );
     return transactions;
   }
 }
